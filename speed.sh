5532805f84724a0e307462f954b8c61e28d15c11
topengs <156810567+topengs@users.noreply.github.com>
2024-07-01 14:54:07 +0700

    Add files via upload

--- speed.sh
+++ speed.sh
@@ -1,6 +1,6 @@
 #codeBYsijhon
 
-a='\033[30;0m' ;h='\033[32;1m' ;m='\033[31;1m' ;c='\033[36;1m' ;p='\033[1;35m' ;k='\033[33;1m' ;b='\033[34;m' ;clear ;echo -e "${p} " ;figlet script ;echo -e "sijhon hanya recode sedikit" ;echo -e "${k}pemilik script👇" ;echo -e "@Zexpo Tweaks" ;echo -e " " ;echo -e "${b} [1]${h} fps boost" ;echo -e "${b} [2]${h} touch boost" ;echo -e "${b} [3]${h} mode gaming" ;echo -e "${b} [4]${h} internet boost" ;echo -e "${b} [5]${h} remove script" ;echo -e "${b} [0]${m} exit tools" ;echo -e "${a} " ;read -p "pilih : " gaming
+a='\033[30;0m' ;h='\033[32;1m' ;m='\033[31;1m' ;c='\033[36;1m' ;p='\033[1;35m' ;k='\033[33;1m' ;b='\033[34;m' ;clear ;echo -e "${p} " ;figlet script ;echo -e "sijhon hanya recode sedikit" ;echo -e "30-06-2024" ;echo -e "${k}pemilik script👇" ;echo -e "@Zexpo Tweaks" ;echo -e " " ;echo -e "${b} [1]${h} fps boost" ;echo -e "${b} [2]${h} touch boost" ;echo -e "${b} [3]${h} mode gaming" ;echo -e "${b} [4]${h} internet boost" ;echo -e "${b} [5]${h} remove script" ;echo -e "${b} [0]${m} exit tools" ;echo -e "${a} " ;read -p "pilih : " gaming
 case $gaming in
 1)
 echo -e "${h}stabil fps in device${b}" ;sleep 1 ;sh Fps_Boost.sh ;read -p "kembali ke menu" back ;bash speed.sh
